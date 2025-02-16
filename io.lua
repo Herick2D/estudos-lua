9c45237339ddad6db092492d055336d2113c2a6b
Herick2D <contato.herickmoreira@gmail.com>
2025-02-16 17:44:26 -0300

    adição de comentários

--- io.lua
+++ io.lua
@@ -3,12 +3,13 @@ local name = io.read() -- Função nativa de entrada e saída 'io'- input / outp
 print(type(name)) -- Todo tipo ao usar o método read() é uma string
 print('Olá', name)
 
+-- Declaração de uma função local
 local function soma(n1, n2)
     return n1 + n2
 end
 
 print('Digite um valor numérico')
-local valor = io.read("*number")
+local valor = io.read("*number") -- conversão do valor recebido via read() para number
 print('Digite outro valor numérico')
 local valor2 = io.read("*number")
 print('A soma dos valores é de: ', soma(valor, valor2))
