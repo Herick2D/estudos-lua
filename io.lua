64c53e1700a5acaa29ebd3fe53130ed865eb5748
Herick2D <contato.herickmoreira@gmail.com>
2025-02-16 18:12:23 -0300

    Estudos input output

--- io.lua
+++ io.lua
@@ -13,3 +13,23 @@ local valor = io.read("*number") -- conversão do valor recebido via read() para
 print('Digite outro valor numérico')
 local valor2 = io.read("*number")
 print('A soma dos valores é de: ', soma(valor, valor2))
+
+--  No teste acima imputar um valor que não pode ser convertido pelo read("*number")
+--  implica em um arithmetic stack traceback
+
+local parte1, parte2, parte3
+parte1 = 'Essa frase'
+parte2 = ' pode ser '
+parte3 = 'concatenada de forma correta'
+
+print(parte1 .. parte2 .. parte3) -- Na linguagem Lua para concaternamos usamos o uso dos ".."
+
+-- Entendendo conceitos da função gsub para trabalhos com string
+local info1, info2, aux
+
+info1 = 'Olá eu me chamo Herick'
+info2 = 'Karina'
+
+aux = string.gsub(info1, 'Herick', info2) -- utilização do método gsub da classe string para trabalhos com string.
+-- gsub espera três parâmetros, sendo o primeiro a string a ser alterada, o segundo parâmetro é o que será substituido e o terceiro parâmetro é o que vai substituir.
+print(aux)
