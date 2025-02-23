c3a84aa1831584546af931e2400ba8fbbd13d30a
Herick2D <contato.herickmoreira@gmail.com>
2025-02-23 14:12:19 -0300

    comentários sobre o lua

--- adivinheNumero.lua
+++ adivinheNumero.lua
@@ -1,11 +1,11 @@
-math.randomseed(os.time())
+math.randomseed(os.time()) -- essa lógica serve para que sempre que o math for usado, ele altere o número, caso contrário o número seria estático (não sei se entendi muito bem, mas o lua parece esquisito nessa parte)
 
-local valor = math.random(100)
-local tentativas = 1
+local valor = math.random(100) -- variável que terá um valor aleatório de 1 a 100
+local tentativas = 1 -- marca as tentativas do usuário
 
 print("Adivinhe o valor")
 local num = io.read("*number")
-while num ~= valor do
+while num ~= valor do -- lógica do jogo
     tentativas = tentativas + 1
     if (num < valor) then
         print("Seu valor foi menor")
